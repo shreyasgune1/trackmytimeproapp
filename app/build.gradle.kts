1c24543882cf55255f66dd897b25ebabfd02eb68
shreyasgune1 <shreyasgune@gmail.com>
2026-04-17 18:59:28 +0530

    Bump versionCode to 4 for API 35 update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.dreamdevelopersone.trackmytimepro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "2.0-pro"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
